733171dd5a7a345a88c856c8ce8ea22131c3fbfd
Nasos <nasos.ibo@gmail.com>
2026-02-09 19:52:37 +0200

    Add PWA support (manifest, service worker, icons)

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'kwh-calc-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './kwh_calc.html',
+    './manifest.json',
+    './icons/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
